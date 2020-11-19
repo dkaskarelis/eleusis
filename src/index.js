19cba7d515eea89b23a2594f2e08d35fcc6f3712
chief <dkaskarelis@gmail.com>
2020-11-19 18:20:42 +0000

    setting up basic open theme

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import { scrollArrow } from "./components/elements/scrollArrow";
+
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import "./App.css";
+import "./assets/scss/style.scss";
+
+const history = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={history}>
+    <App />
+    <scrollArrow />
+  </Router>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
