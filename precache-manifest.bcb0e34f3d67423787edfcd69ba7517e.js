c671b90383a19466e51afa8b353e5758591a4664
chief <dkaskarelis@gmail.com>
2020-11-20 02:47:46 +0000

    commiting everything

---
+++ precache-manifest.bcb0e34f3d67423787edfcd69ba7517e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca8e8dc2ff6227ce87f2e062acafb9a5",
+    "url": "/eleusis/index.html"
+  },
+  {
+    "revision": "b1f3429e577528fcdf06",
+    "url": "/eleusis/static/css/main.9aaa7946.chunk.css"
+  },
+  {
+    "revision": "69974fc79a7b8feadf90",
+    "url": "/eleusis/static/js/2.7253fc4b.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/eleusis/static/js/2.7253fc4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1f3429e577528fcdf06",
+    "url": "/eleusis/static/js/main.8c0ccaf0.chunk.js"
+  },
+  {
+    "revision": "d11169bb38bc9a5a92d4",
+    "url": "/eleusis/static/js/runtime-main.8550d345.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/eleusis/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/eleusis/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/eleusis/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/eleusis/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/eleusis/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/eleusis/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/eleusis/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/eleusis/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/eleusis/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/eleusis/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "ca3bba681a1fd648a1239f56bd66dc42",
+    "url": "/eleusis/static/media/logo.ca3bba68.svg"
+  }
+]);
